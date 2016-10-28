da03b142c746800da21eca2b38d06cda60370ffa
Shun Tsubosaki <s-tsubosaki@nbsi.jp>
2016-10-28 04:41:43 +0000

    golang socket.io server

---
+++ go-socket.io/server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    "github.com/googollee/go-socket.io"
+)
+
+func main() {
+    server, err := socketio.NewServer([]string{"websocket", "polling"})
+    if err != nil {
+        log.Fatal(err)
+    }
+    server.On("connection", func(so socketio.Socket) {
+        log.Println("on connection")
+        so.On("disconnection", func() {
+            log.Println("on disconnect")
+        })
+    })
+    server.On("error", func(so socketio.Socket, err error) {
+        log.Println("error:", err)
+    })
+
+    http.Handle("/socket.io", server)
+    http.ListenAndServe(":8080", nil)
+}
