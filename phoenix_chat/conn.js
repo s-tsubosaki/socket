eba267d8a723e6f623e080ffabdac8eeec820eb6
Shun Tsubosaki <s-tsubosaki@nbsi.jp>
2017-01-25 06:46:41 +0000

    phoenix

--- phoenix_chat/conn.js
+++ phoenix_chat/conn.js
@@ -3,7 +3,15 @@ var max = JSON.parse(fs.readFileSync("../performance/settings.json").toString())
 
 var phoenix = require('phoenix');
 var wb = require('websocket');
+var c = 0;
+
+console.time('connections');
 for (var i = 0; i < max; i++) {
   var socket = new phoenix.Socket("http://127.0.0.1:8080/socket", {transport: wb.w3cwebsocket});
   socket.connect();
+  socket.onOpen( () => {
+    if(++c >= max){
+      console.timeEnd('connections');
+    }
+  } );
 }
