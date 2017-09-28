44143b29eddc054a0559a7ede0958809a3f00cf7
Shun Tsubosaki <s-tsubosaki@nbsi.jp>
2017-09-28 09:00:27 +0000

    listenTCP,listenUDP

--- go/tcp.go
+++ go/tcp.go
@@ -1,37 +1,38 @@
 package main
 
 import (
-    "fmt"
-    "net"
+	"fmt"
+	"net"
 )
 
 func main() {
-    listener, err := net.Listen("tcp", "0.0.0.0:0")
-    if err != nil {
-        fmt.Printf("Listen error: %s\n", err)
-        return
-    }
-    defer listener.Close()
+	tcpAddr, _ := net.ResolveTCPAddr("tcp", ":0")
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		fmt.Printf("Listen error: %s\n", err)
+		return
+	}
+	defer listener.Close()
 
-    addr := listener.Addr()
-    fmt.Printf("Auto binded Port:%d",addr.(*net.TCPAddr).Port)
+	addr := listener.Addr()
+	fmt.Printf("Auto binded Port:%d", addr.(*net.TCPAddr).Port)
 
-    conn, err := listener.Accept()
-    if err != nil {
-        fmt.Printf("Accept error: %s\n", err)
-        return
-    }
-    defer conn.Close()
+	conn, err := listener.AcceptTCP()
+	if err != nil {
+		fmt.Printf("Accept error: %s\n", err)
+		return
+	}
+	defer conn.Close()
 
-    buf := make([]byte, 1024)
-    for {
-        n, err := conn.Read(buf)
-        if n == 0 {
-            break
-        }
-        if err != nil {
-            fmt.Printf("Read error: %s\n", err)
-        }
-        fmt.Print(string(buf[:n]))
-    }
+	buf := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buf)
+		if n == 0 {
+			break
+		}
+		if err != nil {
+			fmt.Printf("Read error: %s\n", err)
+		}
+		fmt.Print(string(buf[:n]))
+	}
 }
--- go/udp.go
+++ go/udp.go
@@ -1,41 +1,38 @@
 package main
 
 import (
-    "log"
-    "net"
-    "os"
+	"log"
+	"net"
+	"os"
 )
 
 func main() {
-    udpAddr := &net.UDPAddr{
-        IP:   net.ParseIP("127.0.0.1"),
-        Port: 0,
-    }
-		udpLn, err := net.ListenUDP("udp", udpAddr)
+	udpAddr, _ := net.ResolveUDPAddr("udp", ":0")
+	udpLn, err := net.ListenUDP("udp", udpAddr)
 
-    if err != nil {
-        log.Fatalln(err)
-        os.Exit(1)
+	if err != nil {
+		log.Fatalln(err)
+		os.Exit(1)
+	}
+
+	laddr := udpLn.LocalAddr()
+	log.Printf("Auto binded Port:%d", laddr.(*net.UDPAddr).Port)
+
+	buf := make([]byte, 1024)
+
+	for {
+		n, addr, err := udpLn.ReadFromUDP(buf)
+		if err != nil {
+			log.Fatalln(err)
+			os.Exit(1)
 		}
-		
-		laddr := udpLn.LocalAddr()
-		log.Printf("Auto binded Port:%d",laddr.(*net.UDPAddr).Port)
 
-    buf := make([]byte, 1024)
+		go func() {
+			log.Printf("Reciving data: %s from %s", string(buf[:n]), addr.String())
 
-    for {
-        n, addr, err := udpLn.ReadFromUDP(buf)
-        if err != nil {
-            log.Fatalln(err)
-            os.Exit(1)
-        }
-
-        go func() {
-            log.Printf("Reciving data: %s from %s", string(buf[:n]), addr.String())
-
-            log.Printf("Sending data..")
-            udpLn.WriteTo([]byte("Pong"), addr)
-            log.Printf("Complete Sending data..")
-        }()
-    }
+			log.Printf("Sending data..")
+			udpLn.WriteTo([]byte("Pong"), addr)
+			log.Printf("Complete Sending data..")
+		}()
+	}
 }
